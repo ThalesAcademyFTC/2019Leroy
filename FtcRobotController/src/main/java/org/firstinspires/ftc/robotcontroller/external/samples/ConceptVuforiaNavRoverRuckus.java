4faa36497e81356689a2cc94a5bcc2efe85c9aec
Dallas Crenshaw <dallas.crenshaw@students.thalesacademy.org>
2018-09-20 07:09:50 -0700

    Pushing CustomClaspy

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuforiaNavRoverRuckus.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuforiaNavRoverRuckus.java
@@ -143,8 +143,7 @@ public class ConceptVuforiaNavRoverRuckus extends LinearOpMode {
         parameters.cameraDirection   = CAMERA_CHOICE;
 
         //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
+        vuforia = ClassFactory.createVuforiaLocalizer(parameters);
         // Load the data sets that for the trackable objects. These particular data
         // sets are stored in the 'assets' part of our application.
         VuforiaTrackables targetsRoverRuckus = this.vuforia.loadTrackablesFromAsset("RoverRuckus");
