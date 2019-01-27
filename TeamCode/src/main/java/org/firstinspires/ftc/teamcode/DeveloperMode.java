5ef084b0acdd1155ef2b374ee59963cf18642c06
Dal0xE <d.crrenshaw21@gmail.com>
2019-01-27 13:46:57 -0500

    Teeny tiny refactor to avoid errors.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeveloperMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeveloperMode.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import fi.iki.elonen.NanoHTTPD;
-import fi.iki.elonen.NanoHTTPD.Response;
 
 /**
  * Created by dcrenshaw on 1/20/19.
@@ -15,6 +14,6 @@ public class DeveloperMode  extends NanoHTTPD {
         super(8080);
     }
     public Response serve(IHTTPSession session) {
-        return Response.newFixedLengthResponse("Coming soon...");
+        return NanoHTTPD.newFixedLengthResponse("Coming soon...");
     }
 }
