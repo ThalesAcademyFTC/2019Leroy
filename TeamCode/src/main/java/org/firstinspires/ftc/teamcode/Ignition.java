9845e83f32b14ab59b09787d5e3cfb5c734644fe
Dal0xE <d.crrenshaw21@gmail.com>
2019-01-27 14:55:17 -0500

    This could cause so many errors it's actually scary.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ignition.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.app.Activity;
+
+/**
+ * Created by dcrenshaw on 1/27/19.
+ *
+ * This is called at start; mostly, it's here to avoid problems.
+ */
+
+public class Ignition extends Activity {
+
+}
